a7ed61c06ec5522791e33fe8b5698e5fbc17f0e8
P jama <pnnjama@gmail.com>
2022-06-15 03:24:33 +0200

    simple interest loan

---
+++ simpleLoan.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class simpleLoan{
+
+public static void (String{}args){
+
+//simpleInterestLoan=SIL
+int SIL=0;
+
+// initialAmount=IA
+int IA=0;
+
+//accumulatedInterestAmount=AIA
+int AIA=0;
+
+//interest rate=i
+
+int i=7;
+
+Scanner input = new Scanner (System.in);
+IA =input.nextInt();
+
+AIA=IA*i/100;
+
+SIL=IA + AIA;
+
+input.nextInt()
+
+System.out.println("the simple interest accumulated by 7% after a month is " + SIL)
+
+}
+}
